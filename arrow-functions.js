2582648d47c31cb2bcdd0b33df3e9c10cb5c5679
Michael Cloran <107754541+michaelcloran@users.noreply.github.com>
2024-03-01 13:33:33 +0000

    arrow-functions
    shows various javaScript arrow functions

---
+++ arrow-functions.js
@@ -0,0 +1,42 @@
+//vanilla JavaScript
+//function addTwoNumbers(a, b){
+    //code block
+  //  return a + b;
+//}
+
+//let sum = addTwoNumbers(3, 5);
+
+
+
+
+//ES6 arrow functions
+const addTwoNumbers = (a, b) => {
+    //code block
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+
+console.log(sum);
+
+//single line arrow functions
+const addTwoNumbers2 = (a, b) => a + b;
+
+let sum2 = addTwoNumbers2(3, 6);
+console.log(sum2);
+
+//implicit functions
+const saySomething = message => console.log(message);
+saySomething("Hello there!!");
+
+const sayHello = () => console.log("Hello")
+sayHello();
+
+//returning multiple lines
+const returnMultipleLines = () => (
+    `<p>
+    This is a multi line string
+    </p>`
+)
+console.log(returnMultipleLines());
+
+
